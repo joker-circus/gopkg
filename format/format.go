3dc2b6531182c20e2ed555914e2753524cc00a85
stephen.li <lifetimeys.li@gmail.com>
2023-07-25 14:41:26 +0800

    style: 调整 format

---
+++ format/format.go
@@ -0,0 +1,14 @@
+package format
+
+import "github.com/joker-circus/gopkg/internal"
+
+var (
+	Json                 = internal.Json
+	JsonIndent           = internal.JsonIndent
+	JsonStruct           = internal.JsonStruct
+	JsonIndentStruct     = internal.JsonIndentStruct
+	JsonIndentBytes      = internal.JsonIndentBytes
+	JsonEscapeHTML       = internal.JsonEscapeHTML
+	JsonEscapeHTMLStruct = internal.JsonEscapeHTMLStruct
+	JsonEscapeHTMLBytes  = internal.JsonEscapeHTMLBytes
+)
